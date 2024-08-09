78ff471cbf7fc2b44cabdc49ea78889d9dacccc6
krezon123 <krezon@yandex.ru>
2024-08-09 14:55:58 +0300

    Homewor5

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+// for(let i = 1; i <= 50; i++){
+//     console.log(i);   
+// }
+
+// for(let i = 50; i >= 25; i--){
+//     console.log(i);
+// }
+
+for (let i = 2500; i <= 3000; i += 1){
+    if (i % 2){
+      console.log(i);
+    }
+  }
+
+
+
